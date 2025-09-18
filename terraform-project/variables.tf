eea26a94e9d9c163c004b93165b50078ff2d38ab
aashwin11backsup <aniketritamashwin@gmail.com>
2025-09-18 21:35:41 +0000

    Infra developed

---
+++ terraform-project/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for the infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "A unique name for the project to prefix resources"
+  type        = string
+  default     = "staging-eks-demo"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for EKS"
+  type        = string
+  default     = "1.31"
+}
