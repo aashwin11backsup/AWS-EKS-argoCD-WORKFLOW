52a6f6e94adb3d837b4bdf63d5c42ccb0e789cc9
aashwin11backsup <aniketritamashwin@gmail.com>
2025-10-03 01:49:05 +0000

    Updated 2nd flow

--- terraform-project/modules/eks/main.tf
+++ terraform-project/modules/eks/main.tf
@@ -1,109 +1,132 @@
 # IAM Role for EKS Cluster
 resource "aws_iam_role" "cluster_role" {
-  name = "${var.project_name}-cluster-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
+    name = "${var.project_name}-cluster-role"
+      assume_role_policy = jsonencode({
+            Version = "2012-10-17"
+                Statement = [
+                        {
+                                  Action = "sts:AssumeRole"
+                                          Effect = "Allow"
+                                                  Principal = {
+                                                              Service = "eks.amazonaws.com"
+                                                  }
+                        },
+                ]
+      })
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  role       = aws_iam_role.cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role       = aws_iam_role.cluster_role.name
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 # EKS Cluster
 resource "aws_eks_cluster" "main" {
-  name     = var.project_name
-  role_arn = aws_iam_role.cluster_role.arn
-  version  = var.cluster_version
+    name     = var.project_name
+      role_arn = aws_iam_role.cluster_role.arn
+        version  = var.cluster_version
 
-  vpc_config {
-    subnet_ids = var.private_subnet_ids
-  }
+          vpc_config {
+                subnet_ids = var.private_subnet_ids
+          }
 
-    access_config {
-    authentication_mode                       = "API"
-    bootstrap_cluster_creator_admin_permissions = true
-  }
+              access_config {
+                    authentication_mode                       = "API"
+                        bootstrap_cluster_creator_admin_permissions = true
+              }
 
-  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
+                depends_on = [aws_iam_role_policy_attachment.cluster_policy]
 }
 
 # IAM Role for Worker Nodes
 resource "aws_iam_role" "node_role" {
-  name = "${var.project_name}-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+    name = "${var.project_name}-node-role"
+      assume_role_policy = jsonencode({
+            Version = "2012-10-17"
+                Statement = [
+                        {
+                                  Action = "sts:AssumeRole"
+                                          Effect = "Allow"
+                                                  Principal = {
+                                                              Service = "ec2.amazonaws.com"
+                                                  }
+                        },
+                ]
+      })
 }
 
 resource "aws_iam_role_policy_attachment" "node_worker_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.node_role.name
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "node_cni_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role       = aws_iam_role.node_role.name
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 # --- ADDITION 1: Attach the ECR Read-Only Policy ---
 resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = aws_iam_role.node_role.name
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 # --- END OF ADDITION 1 ---
 
 
 # EKS Node Group
 resource "aws_eks_node_group" "main" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.project_name}-nodes"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = var.private_subnet_ids
-  instance_types  = var.instance_types
-  
-  scaling_config {
-    desired_size = var.desired_size
-    max_size     = var.max_size
-    min_size     = var.min_size
-  }
+    cluster_name    = aws_eks_cluster.main.name
+      node_group_name = "${var.project_name}-nodes"
+        node_role_arn   = aws_iam_role.node_role.arn
+          subnet_ids      = var.private_subnet_ids
+            instance_types  = var.instance_types
+              
+                scaling_config {
+                      desired_size = var.desired_size
+                          max_size     = var.max_size
+                              min_size     = var.min_size
+                }
 
-  update_config {
-    max_unavailable = 1
-  }
+                  update_config {
+                        max_unavailable = 1
+                  }
 
-  labels = {
-    role = "general-nodes"
-  }
+                    labels = {
+                          role = "general-nodes"
+                    }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.node_worker_policy,
-    aws_iam_role_policy_attachment.node_cni_policy,
-    # --- ADDITION 2: Add a dependency on the new policy attachment ---
-    aws_iam_role_policy_attachment.node_ecr_policy,
-    # --- END OF ADDITION 2 ---
-  ]
+                      depends_on = [
+                            aws_iam_role_policy_attachment.node_worker_policy,
+                                aws_iam_role_policy_attachment.node_cni_policy,
+                                    # --- ADDITION 2: Add a dependency on the new policy attachment ---
+                                        aws_iam_role_policy_attachment.node_ecr_policy,
+                                            # --- END OF ADDITION 2 ---
+                      ]
 
-  lifecycle {
-    ignore_changes = [scaling_config[0].desired_size]
-  }
+                        lifecycle {
+                              ignore_changes = [scaling_config[0].desired_size]
+                        }
+}
+                        }
+                      ]
+                    }
+                  }
+                }
+}
+}
+}
+}
+                                                  }
+                        }
+                ]
+      })
+}
+              }
+          }
+}
+}
+                                                  }
+                        }
+                ]
+      })
 }
